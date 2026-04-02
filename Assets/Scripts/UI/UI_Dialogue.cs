583d9d20ba8190bc58b7ddf54293ed87d162bd7f
Greer Gurdziel <ggurdziel@icloud.com>
2026-04-02 01:47:51 -0400

    Dialogue UI and NPC interaction implementation

---
+++ Assets/Scripts/UI/UI_Dialogue.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Dialogue : MonoBehaviour
+{
+    [SerializeField] private Image speakerPortrait;
+    [SerializeField] private TextMeshProUGUI speakerName;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI dialogueChoices;
+
+    public void PlayDialogueLine(DialogueLineSO line)
+    {
+        speakerPortrait.sprite = line.speaker.speakerPortrait;
+        speakerName.text = line.speaker.speakerName;
+        dialogueText.text = line.GetRandomLine();
+
+    }
+}
